de8db9f7aac92051df416e17beac58a2a0d281d6
lprescott <lukep@nycap.rr.com>
2018-02-02 16:24:57 -0500

    Merged insertionSort into outList class, added comment header to both outList and inList.

--- proj1/src/main/java/csi403/inList.java
+++ proj1/src/main/java/csi403/inList.java
@@ -1,13 +1,45 @@
+//*******************************************************************
+//@author: Luke R. Prescott
+//
+//@file: inList.java ~ The unsorted inList object that will be supplied.
+//
+//CSI 403 – Spring 2018 – Project #1 – Sorting 
+//
+//Goal(s):
+//(1) Establish your development and release environment to be used for class projects. 
+//(2) Implement any one sorting algorithm (e.g. one from the Cormen text).
+//
+//Problem: 
+//(1) Provide a RESTful service which accepts a POST of an array of integers in JSON format and returns the numbers as a JSON object in sorted order.  
+//(2) Any sort algorithm may be used, but you must supply the source code for the sorting algorithm.
+//
+//  Example input:     { “inList”      :    [ 5, 35, 1, 272, 12, 0, -2, 12 ] } 
+//  Example output:    { “outList”     :    [ -2, 0, 1, 5, 12, 12, 35, 272 ], 
+//                       “algorithm”   :    “quicksort”, 
+//                       “timeMS”      :    52 } 
+//
+//*******************************************************************
+
 package csi403;
 
 public class inList {
+	//Properties
 	private int [] inList;
 
+	//Getter(s) and Setter(s)
 	public int [] getInList() {
 		return inList;
 	}
-
 	public void setInList(int [] inList) {
 		this.inList = inList;
+	}
+	
+	//Constructor(s)
+	public inList (int [] list) {
+		this.inList = list;
+	}//End constructor(s)
+	
+	public inList() {
+		//Assign nothing
 	}
 }
--- proj1/src/main/java/csi403/outList.java
+++ proj1/src/main/java/csi403/outList.java
@@ -1,12 +1,37 @@
+//*******************************************************************
+//@author: Luke R. Prescott
+//
+//@file: outList.java ~ The sorted (using insertionSort) outList object that will be returned.
+//
+//CSI 403 – Spring 2018 – Project #1 – Sorting 
+//
+//Goal(s):
+//(1) Establish your development and release environment to be used for class projects. 
+//(2) Implement any one sorting algorithm (e.g. one from the Cormen text).
+//
+//Problem: 
+//(1) Provide a RESTful service which accepts a POST of an array of integers in JSON format and returns the numbers as a JSON object in sorted order.  
+//(2) Any sort algorithm may be used, but you must supply the source code for the sorting algorithm.
+//
+//  Example input:     { “inList”      :    [ 5, 35, 1, 272, 12, 0, -2, 12 ] } 
+//  Example output:    { “outList”     :    [ -2, 0, 1, 5, 12, 12, 35, 272 ], 
+//                       “algorithm”   :    “quicksort”, 
+//                       “timeMS”      :    52 } 
+//
+//*******************************************************************
+
 package csi403;
 
 import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
 
 public class outList implements Serializable{
+	//Properties
 	private int [] outList;
 	private String algorithm;
 	private int timeMS;
 	
+	//Getter(s) and Setter(s)
 	public String getAlgorithm() {
 		return algorithm;
 	}
@@ -25,4 +50,73 @@ public class outList implements Serializable{
 	public void setOutList(int [] outList) {
 		this.outList = outList;
 	}
+	
+	//Constructor(s)
+	public outList (inList list) {
+		//Hard-code algorithm name, "algorithm"
+		
+		//Start timer and then call insertionSort
+		
+		//End timer and then assign "timeMS" with result 
+		//Also assign newly sorted list, "outList"
+	}
+	
+	public outList() {
+		//Assign nothing
+	}
+	
+	//End constructor(s)
+	
+	//This function sorts and inputed integer array by insertion sort.
+	public static int[] sort(int inList[]){
+	  int temp; 
+	  //Traverse the inList.
+	  for (int i = 1; i < inList.length; i++) { 
+	      //Traverse the sorted part of the list (to the right).
+	      for(int j = i ; j > 0 ; j--){
+	          //If the value at pos. j < j - 1, swap them
+	          if(inList[j] < inList[j-1]){
+	                 temp = inList[j];
+	                 inList[j] = inList[j-1];
+	                 inList[j-1] = temp;
+	             }
+	         }
+	     }
+	     return inList;
+	}
+
+	 //This function prints the inputed list on one line for testing.
+	 public static void printList(int list[]){
+	     for (int x = 0; x < list.length; x++){
+	         System.out.print(list[x] + " "); 
+	     }
+	 }
+
+	 //The main method to call insertionSort, and printList methods.
+	 public static void main(String args []){
+	     
+	     //Create and print a list for testing.
+	     System.out.print("The unsorted list:\n");
+	     int testList[] = {13, 2, 1, -5, 6};
+	     printList(testList);
+	     
+	     //Start the timer.
+	     System.out.println("\n\nStarting timer and insertionSort...");
+	     long startTime = System.currentTimeMillis();
+	     
+	     //Call insertionSort.
+	     testList = sort(testList);
+	     
+	     //End the timer.
+	     System.out.println("Done...\n");
+	     long endTime = System.currentTimeMillis();
+
+	     //Calculate the time elapsed.
+	     long elapsedTime = endTime - startTime;
+	     
+	     //Print time elapsed and sorted list.
+	     System.out.println("Elapsed time in milliseconds: " + elapsedTime);
+	     System.out.print("The sorted list:\n");
+	     printList(testList);
+	 }
 }
